b46af65ec2f3ac162c1cc3fed9dd1cc87598c35d
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-11-20 19:27:48 +0600

    mongodb inst

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 
 
 const express =require('express')
+const mongoose = require('mongoose');
 
 const app =express()
 app.use(express.json())
 
+// AiNaWX1i5lMFMSez
+// FirstMongoWork
 
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log(' mongo db Connected!'));
 
 app.get('/', (req ,res)=>{
 
