06f53057a10f14f7e693f893154c9c0b77ce8f38
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-11-20 18:52:42 +0600

    browser

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('server created aas');
+
+
+const express =require('express')
+
+const app =express()
+
+
+
+app.get('/', (req ,res)=>{
+
+    res.send('jadsa')
+
+})
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
