caec5e5e00d158cf2e1239f57b56141870643fa7
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-11-21 11:59:18 +0600

    login and reg complete  and hw complete

--- main.js
+++ main.js
@@ -1,103 +1,120 @@
-
-
-const express =require('express')
+const express = require('express')
 const mongoose = require('mongoose');
-// const { use } = require('react');
 
-const app =express()
+const app = express()
 app.use(express.json())
 
-// AiNaWX1i5lMFMSez
-// FirstMongoWork
+// AiNaWX1i5lMFMSez   pass
+// FirstMongoWork  project name
 
 mongoose.connect('mongodb+srv://FirstMongoWork:AiNaWX1i5lMFMSez@cluster0.7ooynjm.mongodb.net/FirstMongoWork?appName=Cluster0')
-  .then(() => console.log(' mongo db Connected!'));
+    .then(() => console.log(' mongo db Connected!'));
 
-  const Schema = mongoose.Schema;
-
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  Name: String,
-  Email: String,
-  Password: String,
- 
+    Name: String,
+    Email: String,
+    Password: String,
 });
+const user = mongoose.model('AllUser', UserSchema)
 
-const user =mongoose.model('AllUser', UserSchema)
+// --------------------Register section---------------------
+
+app.get('/register', async (req, res) => {
+
+    try {
+
+        const {
+            Name,
+            Email,
+            Password
+        } = req.body;
+
+        if (!Name) return res.status(400).send({
+            Error: 'Name is required'
+        })
+        if (!Email) return res.status(400).send({
+            Error: 'Email is required'
+        })
+        if (!Password) return res.status(400).send({
+            Error: 'Password is required'
+        })
+
+        const existinguser = await user.findOne({
+            Email
+        })
+
+        if (existinguser) return res.status(400).send({
+            Error: 'User already Exists'
+        })
+
+        const newUser = new user({
+            Name,
+            Email,
+            Password
+        })
+
+        newUser.save()
+
+        res.status(201).send({
+            sucess: 'reg successful for user',
+            newUser
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            error: 'reg failed'
+        })
+    }
+})
 
 
+// --------------------login section------------------------
 
-
-
-app.get('/register', async (req ,res)=>{
+app.get('/Login', async (req, res) => {
 
 
     try {
-        
-   const { Name, Email, Password} =req.body ;
+
+        const {
+            Email,
+            Password
+        } = req.body;
 
 
-if(!Name) return res.status(400).send({Error: 'Name is required'})
-if(!Email) return res.status(400).send({Error: 'Email is required'})
-if(!Password) return res.status(400).send({Error: 'Password is required'})
+        const newUser = await user.findOne({
+            Email
+        })
 
+        if (!newUser) return res.status(400).send({
+            Error: 'user does not exist'
+        })
+        if (newUser.Password !== Password) return res.status(400).send({
+            Error: 'Password dosent match'
+        })
 
-const existinguser = await user.findOne({Email})
+        res.status(201).send({
+            sucess: 'login successful ',
+            newUser
+        })
 
-if(existinguser) return res.status(400).send({Error: 'User already Exists'})
-
-   const newUser =new user ({
-    Name,
-    Email,
-    Password
-   })
-
-newUser.save()
-
-    res.status(201).send( { sucess:'reg successful for user' , newUser  })
-        
     } catch (error) {
-        res.status(500).send({error:'reg failed'})
+        res.status(500).send({
+            error: 'login failed'
+        })
     }
 
 
 })
 
-
-
-app.get('/Login', async  (req ,res)=>{
-
-
-    try {
-        
-   const { Email, Password} =req.body ;
-
-
-   const newUser = await user.findOne({Email}) 
-
-if(!newUser) return res.status(400).send({Error: 'user does not exist'})
-if(newUser.Password !== Password) return res.status(400).send({Error: 'Password dosent match'})
-
-
-
-
-
-    res.status(201).send( { sucess:'login successful ' , newUser  })
-        
-    } catch (error) {
-        res.status(500).send({error:'login failed'})
-    }
-
-
-})
-
-
-
-app.get('/', (req ,res)=>{
+// -----------------home section------------
+app.get('/', (req, res) => {
 
     res.send('')
 
 })
+// ---------------------server seaction---------------
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
