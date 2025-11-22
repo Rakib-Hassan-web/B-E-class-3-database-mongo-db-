2b4752608f9e0eb196fcc07aa435e1a106918d31
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-11-22 23:41:04 +0600

    practice database

--- main.js
+++ main.js
@@ -1,121 +1,128 @@
-const express = require('express')
-const mongoose = require('mongoose');
+// const express = require('express')
+// const mongoose = require('mongoose');
 
-const app = express()
-app.use(express.json())
+// const app = express()
+// app.use(express.json())
 
-// AiNaWX1i5lMFMSez   pass
-// FirstMongoWork  project name
+// // AiNaWX1i5lMFMSez   pass
+// // FirstMongoWork  project name
 
-mongoose.connect('mongodb+srv://FirstMongoWork:AiNaWX1i5lMFMSez@cluster0.7ooynjm.mongodb.net/FirstMongoWork?appName=Cluster0')
-    .then(() => console.log(' mongo db Connected!'));
+// mongoose.connect('mongodb+srv://FirstMongoWork:AiNaWX1i5lMFMSez@cluster0.7ooynjm.mongodb.net/FirstMongoWork?appName=Cluster0')
+//     .then(() => console.log(' mongo db Connected!'));
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    Name: String,
-    Email: String,
-    Password: String,
-});
-const user = mongoose.model('AllUser', UserSchema)
+// const UserSchema = new Schema({
+//     Name: String,
+//     Email: String,
+//     Password: String,
+// });
+// const user = mongoose.model('AllUser', UserSchema)
 
-// --------------------Register section---------------------
+// // --------------------Register section---------------------
 
-app.get('/register', async (req, res) => {
+// app.get('/register', async (req, res) => {
 
-    try {
+//     try {
 
-        const {
-            Name,
-            Email,
-            Password
-        } = req.body;
+//         const {
+//             Name,
+//             Email,
+//             Password
+//         } = req.body;
 
-        if (!Name) return res.status(400).send({
-            Error: 'Name is required'
-        })
-        if (!Email) return res.status(400).send({
-            Error: 'Email is required'
-        })
-        if (!Password) return res.status(400).send({
-            Error: 'Password is required'
-        })
+//         if (!Name) return res.status(400).send({
+//             Error: 'Name is required'
+//         })
+//         if (!Email) return res.status(400).send({
+//             Error: 'Email is required'
+//         })
+//         if (!Password) return res.status(400).send({
+//             Error: 'Password is required'
+//         })
 
-        const existinguser = await user.findOne({
-            Email
-        })
+//         const existinguser = await user.findOne({
+//             Email
+//         })
 
-        if (existinguser) return res.status(400).send({
-            Error: 'User already Exists'
-        })
+//         if (existinguser) return res.status(400).send({
+//             Error: 'User already Exists'
+//         })
 
-        const newUser = new user({
-            Name,
-            Email,
-            Password
-        })
+//         const newUser = new user({
+//             Name,
+//             Email,
+//             Password
+//         })
 
-        newUser.save()
+//         newUser.save()
 
-        res.status(201).send({
-            sucess: 'reg successful for user',
-            newUser
-        })
+//         res.status(201).send({
+//             sucess: 'reg successful for user',
+//             newUser
+//         })
 
-    } catch (error) {
-        res.status(500).send({
-            error: 'reg failed'
-        })
-    }
-})
-
-
-// --------------------login section------------------------
-
-app.get('/Login', async (req, res) => {
+//     } catch (error) {
+//         res.status(500).send({
+//             error: 'reg failed'
+//         })
+//     }
+// })
 
 
-    try {
+// // --------------------login section------------------------
 
-        const {
-            Email,
-            Password
-        } = req.body;
+// app.get('/Login', async (req, res) => {
 
 
-        const newUser = await user.findOne({
-            Email
-        })
+//     try {
 
-        if (!newUser) return res.status(400).send({
-            Error: 'user does not exist'
-        })
-        if (newUser.Password !== Password) return res.status(400).send({
-            Error: 'Password dosent match'
-        })
-
-        res.status(201).send({
-            sucess: 'login successful ',
-            newUser
-        })
-
-    } catch (error) {
-        res.status(500).send({
-            error: 'login failed'
-        })
-    }
+//         const {
+//             Email,
+//             Password
+//         } = req.body;
 
 
-})
+//         const newUser = await user.findOne({
+//             Email
+//         })
 
-// -----------------home section------------
-app.get('/', (req, res) => {
+//         if (!newUser) return res.status(400).send({
+//             Error: 'user does not exist'
+//         })
+//         if (newUser.Password !== Password) return res.status(400).send({
+//             Error: 'Password dosent match'
+//         })
 
-    res.send('')
+//         res.status(201).send({
+//             sucess: 'login successful ',
+//             newUser
+//         })
 
-})
-// ---------------------server seaction---------------
+//     } catch (error) {
+//         res.status(500).send({
+//             error: 'login failed'
+//         })
+//     }
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+
+// })
+
+// // -----------------home section------------
+// app.get('/', (req, res) => {
+
+//     res.send('')
+
+// })
+// // ---------------------server seaction---------------
+
+// app.listen(4000, () => {
+//     console.log('Server is running on port 4000');
+// });
+
+
+
+
+
+
+console.log('hellawo')
